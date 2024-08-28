0e1f77dfc8059871f1ddf96e2214df395b25729a
CodeWard <mahmoud.elshony43@gmail.com>
2024-08-28 12:52:17 +0300

    remove emojy from comments

--- models/collections.js
+++ models/collections.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-// this function create a schema for collections 😂😂
+// this function create a schema for collections
 const collectionsSchema = new mongoose.Schema(
   {
     user_id: {
